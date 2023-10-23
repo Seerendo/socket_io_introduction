adc364c14f6b148c0184da5fe1990381049e3fdd
Seerendo <70487872+Seerendo@users.noreply.github.com>
2023-10-23 16:02:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## DEPENDENCIAS
+
+"express": "^4.18.2",
+"moongose": "^1.0.0",
+"socket.io": "^4.7.2",
+"nodemon": "^3.0.1"
+
+# RESOURCES
+
+Backend simple para una aplicación de ejemplo de un chat con flutter como app frontend usando Socket.IO
+Frontend: https://github.com/Seerendo/socket_flutter_chat
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.route('/').get((req, res) => {
+  res.json('Hey there welcome again on dev stack channel');
+});
+
+io.on('connection', (socket) => {
+  //Socket group
+  socket.join('anomyous_group');
+  console.log('backend connected');
+  socket.on('sendMsg', (msg) => {
+    console.log('msg', msg /* { ...msg, type: 'otherMsg' } */);
+    /* socket.emit('sendMsgServer', { ...msg, type: 'otherMsg' }); */
+    io.to('anomyous_group').emit('sendMsgServer', { ...msg, type: 'otherMsg' });
+  });
+});
+
+httpServer.listen(3000);
